a01f915e7dc5550a9f9e755a7ce6ef14868592a8
judi <judkoffi@gmail.com>
2020-05-27 16:22:02 +0200

    update javadoc

--- chathack/src/fr/upem/chathack/visitor/IPublicFrameVisitor.java
+++ chathack/src/fr/upem/chathack/visitor/IPublicFrameVisitor.java
@@ -14,20 +14,62 @@ import fr.upem.chathack.publicframe.ServerResponseMessage;
  */
 public interface IPublicFrameVisitor extends IFrameVisitor {
 
+  /**
+   * Method use to handle a anonymous client connection message received by server
+   * 
+   * @param message: a {@link AnonymousConnection} object
+   */
   public default void visit(AnonymousConnection message) {}
 
+  /**
+   * Method use to handle a authenticated client connection message received by server
+   * 
+   * @param message: a {@link AuthentificatedConnection} object
+   */
   public default void visit(AuthentificatedConnection message) {}
 
+  /**
+   * Method use to handle a public message from a client received by server
+   * 
+   * @param message: a {@link BroadcastMessage} object
+   */
   public void visit(BroadcastMessage message);
 
+  /**
+   * Method use to handle a message send by server to a client
+   * 
+   * @param message: a {@link ServerResponseMessage} object
+   */
   public default void visit(ServerResponseMessage serverMessage) {}
 
+  /**
+   * Method use to handle a private connection between client request message by server or a client
+   * 
+   * @param message: a {@link RequestPrivateConnection} object
+   */
   public void visit(RequestPrivateConnection requestMessage);
 
+  /**
+   * Method use to handle a positive response about a private connection request initiate by a
+   * client
+   * 
+   * @param message: a {@link AcceptPrivateConnection} object
+   */
   public void visit(AcceptPrivateConnection responsePrivateConnection);
 
+  /**
+   * Method use to handle a negative response about a private connection request initiate by a
+   * client
+   * 
+   * @param message: a {@link RejectPrivateConnection} object
+   */
   public void visit(RejectPrivateConnection rejectPrivateConnection);
 
+  /**
+   * Method use to handle a disconnection of a client from server
+   * 
+   * @param message: a {@link LogOutMessage} object
+   */
   public void visit(LogOutMessage disconnectionMessage);
 
 }
